fab847019ee66204c46ac6522aaea81b336eb3c9
sota-onozuka <sonta.afcot37@gmail.com>
2021-07-24 17:35:22 +0900

    memoアプリ作成

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,109 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <button v-on:click="addNewNote" class="new-button">Take a new note</button>
+  <div class="notes-area">
+    <ul class="notes">
+      <li v-for="(note, index) in notes" :key= "note.id" class="note">
+        <p class="title">{{note.title}}</p>
+        <button v-on:click="setEditInfo(index)" class="show-button">Show</button>
+        <button v-on:click="removeNote(index)" class="delete-button">Delete</button>
+      </li>
+    </ul>
+  </div>
+  <Note
+    v-if="editIndex != -1"
+    :focusedId="editIndex"
+    :content="notes[editIndex].content || ''"
+    :title="notes[editIndex].title || ''"
+    :paramtitle="param1"
+    :paramcontent="param2"
+    v-on:update:paramtitle="param1 = $event"
+    v-on:update:paramcontent="param2 = $event"
+    v-on:editnote="editNote"
+  ></Note>
+    <button v-if="editIndex != -1" v-on:click="editIndex=-1" class="button-footer">Finish</button>
+    <button v-if="editIndex != -1" v-on:click="removeNote(editIndex)" class="button-footer">Delete</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Note from "./components/Note.vue";
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Note,
+  },
+  data() {
+    return {
+    targetNoteContent: '',
+    targetNoteTitle: '',
+    editIndex: -1,
+    notes: [
+    ],
+    nextNoteId: 0,
+    param1: '',
+    param2: '',
+    }
+  },
+  props: ['note'],
+  mounted() {
+    this.notes = JSON.parse(localStorage.getItem('access_notes')) || [];
+    var ids = [];
+    for (const note of this.notes){
+      ids.push(note.id);
+    }
+    const aryMax = function (a, b) {return Math.max(a, b);}
+    let maxId = ids.reduce(aryMax);
+    this.nextNoteId = maxId + 1;
+  },
+  methods: {
+    addNewNote() {
+      this.editIndex = this.notes.length ;
+      this.param1 = ''
+      this.param2 = ''
+      this.notes.push(
+        {
+          id: this.nextNoteId++,
+          title: '',
+          content: '',
+        });
+      this.saveNotes()
+    },
+    editNote() {
+      this.notes.splice(this.editIndex, 1, {
+        id: this.notes[this.editIndex].id,
+        title: this.param1,
+        content: this.param2,
+      });
+      this.saveNotes()
+    },
+    setEditInfo(index) {
+      this.param1 = this.notes[index].title
+      this.param2 = this.notes[index].content
+      this.editIndex = index;
+      this.targetNoteTitle = this.notes[index].title;
+      this.targetNoteContent = this.notes[index].content;
+    },
+    removeNote(index) {
+      this.notes.splice(index, 1);
+      this.saveNotes();
+      this.editIndex = -1;
+    },
+    saveNotes() {
+      localStorage.setItem('access_notes', JSON.stringify(this.notes));
+    },
+    resetInfo() {
+      this.targetNoteContent = '';
+      this.editIndex = -1;
+    },
+    saveState() {
+      this.notes.splice(this.editIndex, 1, {
+        id: this.editIndex,
+        title: this.param1,
+        content: this.param2,
+      });
+    }
+  },
 }
 </script>
 
@@ -22,5 +115,80 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  font-family: "游ゴシック体", "YuGothic", "游ゴシック", "Yu Gothic", "ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", "メイリオ", "Meiryo", sans-serif;
+}
+.notes-area{
+  height: 300px;
+  width: 60%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 15px;
+  margin-bottom: 10px;
+  overflow-y: scroll;
+}
+.note 
+{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.title {
+  font-size: 15px;
+  width: 40%;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+}
+button {
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  font-size: 15px;
+  border-radius: 3px;
+  background:#383838;
+  color:#fff;
+  border:none;
+  position:relative;
+  cursor:pointer;
+  transition:800ms ease all;
+  outline:none;
+}
+/* かっこいいボタンデザイン(コピー) */
+button:hover{
+  background:#fff;
+  color:#383838;
+}
+button:before, button:after{
+  content:'';
+  position:absolute;
+  top:0;
+  right:0;
+  height:2px;
+  width:0;
+  background: #383838;
+  transition:400ms ease all;
+}
+button:after{
+  right:inherit;
+  top:inherit;
+  left:0;
+  bottom:0;
+}
+button:hover:before, button:hover:after{
+  width:100%;
+  transition:800ms ease all;
+}
+/* かっこいいボタンデザイン終わり */
+button.show-button {
+  margin-left: 10px;
+  margin-right: 10px;
+}
+
+button.button-footer {
+  display: block;
+  margin-top: 15px;
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
---
+++ src/components/Note.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="note-detail">
+    <div class="titles">
+      <p class="caption">Title:</p>
+      <input v-model="setTitle" type="text" class="input-title">
+    </div>
+    <div class="contents">
+      <p class="caption">Content:</p>
+      <textarea v-model="setContent" class="input-content"></textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Note",
+  props: { content: String, title: String, focusedId: Number },
+  computed: {
+    setTitle: {
+      get() {
+        return this.title;
+      },
+      set(title) {
+        this.$emit("update:paramtitle", title);
+        this.$emit('editnote')
+      },
+    },
+    setContent: {
+      get() {
+        return this.content;
+      },
+      set(content) {
+        this.$emit("update:paramcontent", content);
+        this.$emit('editnote')
+      },
+    },
+  },
+};
+</script>
+
+<style>
+.note-detail{
+  width: 60%;
+  display: inline-block;
+  border: solid;
+  border-width: 1px;
+  border-radius: 5px;
+}
+.titles, .contents {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+.contents {
+  align-items: flex-start;
+}
+.input-title, .input-content {
+  border:none;
+  border-width: 1px;
+  border-radius: 5px;
+  border-color: #383838;
+  width:50%;
+  height:50px;
+  font-family: "游ゴシック体", "YuGothic", "游ゴシック", "Yu Gothic", "ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", "メイリオ", "Meiryo", sans-serif;
+}
+.input-content {
+  height:100px;
+  resize: none;
+  font-family: "游ゴシック体", "YuGothic", "游ゴシック", "Yu Gothic", "ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", "メイリオ", "Meiryo", sans-serif;
+}
+.caption {
+  margin: 0px;
+  text-align: right;
+  line-height: 30px;
+  padding-right: 10px;
+  width: 80px;
+  color: #383838;
+}
+/* スクロールバーがかっこよくないのでコピペでいじった */
+::-webkit-scrollbar{
+  width: 10px;
+}
+::-webkit-scrollbar-track{
+  background: #fff;
+  border: none;
+  border-radius: 10px;
+  box-shadow: inset 0 0 2px #777; 
+}
+::-webkit-scrollbar-thumb{
+  background: #ccc;
+  border-radius: 10px;
+  box-shadow: none;
+}
+/* スクロールバーの設定終わり */
+</style>
